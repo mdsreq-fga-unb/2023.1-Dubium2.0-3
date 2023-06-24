5844d054b1cf4adf3a1279d592149c0fe2d32254
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-24 18:13:55 -0300

    .

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -7,7 +7,7 @@ const http = require('./server.js')
 require("./config/sockets.js")
 
 
-const port = 8080
+const port = process.env.PORT
 
 const server = http.listen(port, () => {
     console.log(`O servidor está rodando em: http://localhost:${port}`)
